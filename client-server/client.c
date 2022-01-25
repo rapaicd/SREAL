badbf84aed135c99dd53353d854a9695fb1aeb95
rapaicd <rapaic.danilo9@gmail.com>
2022-01-25 08:38:07 +0100

    client-server com

--- client-server/client.c
+++ client-server/client.c
@@ -35,13 +35,13 @@ int main(int argc, char *argv[])
        return 1;
     }
     memset(&serv_addr, 0, sizeof(serv_addr));
-   
+
     /* kao i podatke neophodne za komunikaciju sa serverom */
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(5001);
 
     /* inet_pton konvertuje ip adresu iz stringa u format
-	neophodan za serv_addr strukturu */	
+	neophodan za serv_addr strukturu */
     if(inet_pton(AF_INET, argv[1], &serv_addr.sin_addr)<=0)
     {
         printf("\n inet_pton error occured\n");
@@ -55,20 +55,54 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    printf("Connected to server... Send message to server, or type 'quit' to exit\n");
+    printf("Connected to server... Send message to server, or type 'ENDE' to exit\n");
 
     /* udji u petlju u kojoj ces slati poruke server sve dok ne posaljes “quit” */
     done = 0;
+	char *agents[]={"JAMESBOND","JANISJOPLIN","SLASH","FREDY","SAMUEL","OZZY","AXLROSE",
+			"ANGUSYOUNG","BLACKMORE","BONSCOTT"};
+	char message[100], ch;
+	int j, key;
+	char tmp[30];
+	/*printf("Unesi kljuc: %d\n");
+	scanf("%d",&key);*/
+	key=4;
+	int k,br=0;
     while (!done){
 	nread=getline(&line, &len, stdin);
        //ignore '\n' 
 	nread--;
 	line[nread] = '\0';
-	if (strcmp(line,"quit") == 0){
-		done = 1;	
+
+
+	for(k=0;k<10;k++){
+		if(strcmp(line,agents[k])==0){
+			br++;
+		}
+	}
+	if(strcmp(line,"NEEDINFO")==0){
+		br++;
+	}
+
+	if (strcmp(line,"ENDE") == 0){
+		done = 1;
+        }else if(br==0){
+		done = 1;
 		printf("Closing connection with the server..\n");
-        }
+	}
+	br=0;
 	/* posalji poruku serveru */
+
+	for(j=0;line[j] != '\0';++j){
+		ch=line[j];
+		if(ch >= 'A' && ch <= 'Z'){
+			ch = ch+key;
+			if(ch > 'Z'){
+				ch = ch - 'Z'+ 'A' -1;
+			}
+			line[j] = ch;
+		}
+	}
 	write (sockfd, line, strlen(line));
     }
     close(sockfd);
--- client-server/server.c
+++ client-server/server.c
@@ -7,36 +7,89 @@
 #include <string.h>
 #include <unistd.h>
 
-	
 
+int agents(int sock){
+
+	char buffer[256];
+	char *agenti[]={"DUSANPOPOVSRBIJA","OLIVERSAKACMADJARSKA","DANILORAPAICAUSTRIJA"
+			,"MARKOMARKOVICIZRAEL","SIMOLAZAREVICNEMACKA","STEFANSIMICENGLESKA",
+			"STEFANVRTIPRASKIMAKEDONIJA","DARKODIMICRUSIJA","LAZAMARKOVICAMERIKA",
+			"SINISAVELEMIROVICSPANIJA"};
+	char *alterEgo[]={"JAMESBOND","JANISJOPLIN","SLASH","FREDY","SAMUEL","OZZY","AXLROSE",
+			"ANGUSYOUNG","BLACKMORE","BONSCOTT"};
+	int n,i;
+	bzero(buffer,256);
+	n = read(sock,buffer,255);
+	buffer[n]=0;
+	int j, key=4;
+	char ch;
+
+	for(j=0;buffer[j] != '\0';++j){
+		ch=buffer[j];
+		if(ch >= 'A' && ch <= 'Z'){
+			ch = ch-key;
+			if(ch < 'A'){
+				ch = ch + 'Z' - 'A' + 1;
+			}
+			buffer[j] = ch;
+		}
+	}
+
+
+	for(i=0;i<10;i++){
+		if(strcmp(buffer,alterEgo[i])==0){
+			printf("%s\n",agenti[i]);
+			return 0;
+		}
+	}
+	printf("AlterEgo se ne poklapa, KOMUNIKACIJA SE PREKIDA!\n");
+	return 5;
+}
 /*ovo je jezgro serverske funkcionalnosti-ova funkcija se poziva
 nakon uspostavljanja veze sa klijentom kako bi klijentu bilo
 omoguceno da komunicira sa serverom. */
 void doprocessing (int sock)
 {
-    /*lokalne promenljive*/	
+    /*lokalne promenljive*/
     int n;
     char buffer[256];
     char sendBuf[256];
     char tempstr[256];
-
     bzero(buffer,256);
     int done = 0;
-	
-
+	char ch;
+	int j,m, key=4;
     while (!done)
     {
     	n = read(sock,buffer,255);
-	buffer[n] = 0;//terminiraj string primljen od strane klijenta
-	if (strcmp(buffer,"quit") == 0){
-		done = 1;			
-		printf("Client closed connection..\n");
-        }
-	else
-	{
-		printf("Received: %s\n",buffer);
+	for(j=0;buffer[j] != '\0';++j){
+		ch=buffer[j];
+		if(ch >= 'A' && ch <= 'Z'){
+			ch = ch-key;
+			if(ch < 'A'){
+				ch = ch + 'Z' - 'A' + 1;
+			}
+			buffer[j] = ch;
+		}
 	}
 
+	buffer[n] = 0;
+
+	if(strcmp(buffer,"NEEDINFO")==0){
+
+		printf("YOUCANGETINFO\n");
+		m = agents(sock);
+		if(m==5){
+			done=1;
+		}
+	}else if(strcmp(buffer,"ENDE")==0){
+
+		done=1;
+		printf("ENDE\n");
+	}else{
+		done=1;
+		printf("KOMUNIKACIJA SE PREKIDA!\n");
+	}
     }
 }
 
